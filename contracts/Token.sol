0f9c6cd281fa34d3af4ab9759985fdf2d12fb7ee
graphica99 <mendsalbert@gmail.com>
2022-03-29 11:58:22 +0000

    dex token exchange

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Token {
-    string public name = "My Name";
+    using SafeMath for uint;
+    string public name = "Hyadum Coin";
+    string public symbol = "HYC";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    //track balances
+    mapping(address => uint256) public balanceOf;
+
+    //send tokens
+
+    constructor (){
+      totalSupply = 1000000 * (10 ** decimals);
+      balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transferTo(address _to, uint _value)public returns (bool success) {
+    //  require(totalSupply < 0 , 'not sufficient balance');
+     balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+     balanceOf[_to] = balanceOf[_to].add(_value);
+     return true;
+    }
+
+
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -7,22 +7,59 @@ describe("Token", function () {
   let owner;
   let addr1;
   let addr2;
-  let addrs;
+  let name = "Hyadum Coin";
+  let decimal = 18;
+  let symbol = "HYC";
+  let totalSupply = "1000000000000000000000000";
 
   beforeEach(async function () {
     Token = await ethers.getContractFactory("Token");
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    [owner, addr1, addr2] = await ethers.getSigners();
     token = await Token.deploy();
     await token.deployed();
   });
 
   it("Should have a name", async function () {
-    expect(await token.name()).to.equal("My Name");
+    expect(await token.name()).to.equal(name);
   });
 
-  it("Should have a symbol", async function () {});
+  it("Should have a symbol", async function () {
+    expect(await token.symbol()).to.equal(symbol);
+  });
 
-  it("Should have a decimal", async function () {});
+  it("Should have a decimal", async function () {
+    expect(await token.decimals()).to.equal(decimal);
+  });
 
-  it("Should have a total supply", async function () {});
+  it("Should have a total supply", async function () {
+    expect(await token.totalSupply()).to.equal(totalSupply);
+  });
+
+  it("Should assign totalSupply to deployer", async function () {
+    expect(await token.balanceOf(owner.address)).to.equal(totalSupply);
+  });
+
+  describe("Transfer Tokens", () => {
+    it("Transfer token balance", async function () {
+      //check if the balance was deducted from total balance
+      let balanceOwner;
+      let balanceReciever;
+
+      balanceOwner = await token.balanceOf(owner.address);
+      console.log("before tx", balanceOwner);
+      balanceReciever = await token.balanceOf(addr1.address);
+      console.log("before tx", balanceReciever);
+
+      await token.transferTo(addr1.address, "100000000000000000000", {
+        from: owner.address,
+      });
+
+      balanceOwner = await token.balanceOf(owner.address);
+      console.log("after tx", balanceOwner);
+      balanceReciever = await token.balanceOf(addr1.address);
+      console.log("after tx", balanceReciever);
+
+      // expect(await token.balanceOf(owner.address)).to.equal(totalSupply);
+    });
+  });
 });
