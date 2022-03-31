90ec6d415cef2ee2e3b69d66adbb920cccbafc7a
graphica99 <mendsalbert@gmail.com>
2022-03-31 06:37:44 +0000

    dex token exchange

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -114,16 +114,11 @@ contract Exchange {
         uint _feeAmount = _amountGive.mul(feePercent).div(100);
       
         tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
-        console.log(balanceOf(_tokenGet,msg.sender));
         tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
-        console.log(balanceOf(_tokenGet,_user));
         tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
         tokens[_tokenGive][_user]= tokens[_tokenGive][_user].sub(_amountGive);
-        console.log(_user);
-        console.log(msg.sender);
-        console.log(balanceOf(_tokenGive,_user));
         tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
-        console.log(balanceOf(_tokenGive,msg.sender));
+     
        
         emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, block.timestamp);
      }
