8899c774c8a2a91762de8c1b60b09f87b45710c1
graphica99 <mendsalbert@gmail.com>
2022-03-31 18:12:47 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="bg-[#1A1D26] w-full h-full">
+    <div className="bg-[#1A1D26] w-full h-full font-Montserrat">
       <nav className="bg-[#20232C] px-4 py-4 w-full flex flex-row justify-between items-center">
-        <p>0xafd....88e8f</p>
+        <p className="text-white text-lg">0xafd....88e8f</p>
         <div className="bg-white rounded-full w-8 h-8"></div>
       </nav>
     </div>
