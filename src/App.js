85ef36c767a227b370d5b547c7851e57438e77c5
graphica99 <mendsalbert@gmail.com>
2022-03-31 22:44:28 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ export default function App() {
                 <div>
                   <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table class="w-full text-sm text-left  text-gray-500 dark:text-gray-400">
-                      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-[#20232C] dark:text-gray-400">
+                      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-[#1A1D26] dark:text-gray-400">
                         <tr>
                           <th scope="col" class="px-6 py-3"></th>
                           <th scope="col" class="px-6 py-3">
@@ -100,17 +100,17 @@ export default function App() {
                         </tr>
                       </thead>
                       <tbody>
-                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-[#20232C] even:dark:bg-[#1A1D26]">
                           <td class="px-6 py-4">9:00am</td>
                           <td class="px-6 py-4">20</td>
                           <td class="px-6 py-4">0.01</td>
                         </tr>
-                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-[#20232C] even:dark:bg-[#1A1D26]">
                           <td class="px-6 py-4">9:00am</td>
                           <td class="px-6 py-4">20</td>
                           <td class="px-6 py-4">0.01</td>
                         </tr>
-                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-[#20232C] even:dark:bg-[#1A1D26]">
                           <td class="px-6 py-4">9:00am</td>
                           <td class="px-6 py-4">20</td>
                           <td class="px-6 py-4">0.01</td>
