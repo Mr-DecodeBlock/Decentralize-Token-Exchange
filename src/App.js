08eaa3928b6ae179bfcd84bd18acf1462e04b28b
graphica99 <mendsalbert@gmail.com>
2022-03-31 18:02:41 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="bg-[#1A1D26] w-full h-full">
-      <nav className="bg-[#20232C] w-full flex flex-row justify-center items-center">
+      <nav className="bg-[#20232C] px-4 py-4 w-full flex flex-row justify-between items-center">
         <p>0xafd....88e8f</p>
+        <div className="bg-white rounded-full w-8 h-8"></div>
       </nav>
     </div>
   );
