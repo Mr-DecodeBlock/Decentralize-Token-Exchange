cf8eaed911e69c8d6f46ad197873a34ea0883e7f
graphica99 <mendsalbert@gmail.com>
2022-03-31 22:53:51 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -122,7 +122,44 @@ export default function App() {
               ) : (
                 ""
               )}
-              {openTab === 2 ? <div>Order content</div> : ""}
+              {openTab === 2 ? (
+                <div>
+                  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left  text-gray-500 dark:text-gray-400">
+                      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-[#1A1D26] dark:text-gray-400">
+                        <tr>
+                          <th scope="col" class="px-6 py-3"></th>
+                          <th scope="col" class="px-6 py-3">
+                            DDAP
+                          </th>
+                          <th scope="col" class="px-6 py-3">
+                            DDAP/ETH
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-[#20232C] even:dark:bg-[#1A1D26]">
+                          <td class="px-6 py-4">9:00am</td>
+                          <td class="px-6 py-4">20</td>
+                          <td class="px-6 py-4">0.01</td>
+                        </tr>
+                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-[#20232C] even:dark:bg-[#1A1D26]">
+                          <td class="px-6 py-4">9:00am</td>
+                          <td class="px-6 py-4">20</td>
+                          <td class="px-6 py-4">0.01</td>
+                        </tr>
+                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-[#20232C] even:dark:bg-[#1A1D26]">
+                          <td class="px-6 py-4">9:00am</td>
+                          <td class="px-6 py-4">20</td>
+                          <td class="px-6 py-4">0.01</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
             <div className="bg-[#20232C] rounded-md text-white">
               <p>transactions</p>
