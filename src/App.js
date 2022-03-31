601e1e1dd3832fba247631c7ab8f01fa1a40be9d
graphica99 <mendsalbert@gmail.com>
2022-03-31 17:42:28 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  export default function App() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
