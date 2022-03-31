a67d55abb3d1267f681f5067d25d4251b45c163e
graphica99 <mendsalbert@gmail.com>
2022-03-31 18:01:10 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="bg-[#1A1D26]">
-      <nav></nav>
+    <div className="bg-[#1A1D26] w-full h-full">
+      <nav className="bg-[#20232C] w-full flex flex-row justify-center items-center">
+        <p>0xafd....88e8f</p>
+      </nav>
     </div>
   );
 }
