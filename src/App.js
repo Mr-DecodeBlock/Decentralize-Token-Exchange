34b96527a79bc55cebcc1ab23f531141a3de7273
graphica99 <mendsalbert@gmail.com>
2022-03-31 18:44:46 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ export default function App() {
   return (
     <div className="bg-[#1A1D26] w-full h-screen font-Montserrat">
       <div>
-        <nav className="bg-[#20232C]  sm:w-full lg:w-3/12 lg:h-screen ">
-          <div className="px-4 sm:py-4 sm:flex sm:flex-row sm:justify-between sm:items-center lg:flex-col-reverse lg:justify-end lg:py-8 ">
+        <nav className="bg-[#20232C] lg:p-4 sm:w-full lg:w-3/12 lg:h-screen ">
+          <div className="px-4 py-4 flex flex-row justify-between items-center lg:flex-col-reverse lg:justify-end lg:py-8 ">
             <p className="text-white text-lg">0xafd....88e8f</p>
             <img className="lg:mb-4" src={avtr} alt="Display" width={40} />
           </div>
-          <div className="w-full">
-            <p>FdsfdfFdsfdfFdsfdfFdsfdfFdsfdfFdsfdfFdsfdfFdsfdf</p>
+          <div>
+            <div className="hidden lg:flex w-full h-24 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md"></div>
           </div>
         </nav>
       </div>
