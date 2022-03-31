791440b42bb33f779115dc0fd0c883b4bdaf0c2c
graphica99 <mendsalbert@gmail.com>
2022-03-31 18:23:24 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from "./logo.svg";
+import avtr from "./avrt-image.png";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="bg-[#1A1D26] w-full h-full font-Montserrat">
+    <div className="bg-[#1A1D26] w-full h-screen font-Montserrat">
       <nav className="bg-[#20232C] px-4 py-4 w-full flex flex-row justify-between items-center">
         <p className="text-white text-lg">0xafd....88e8f</p>
 
         {/* <div className="bg-avrt-image rounded-full w-8 h-8"></div> */}
-        <img className="" src={require("./avrt-image.jpg")} alt="Display" />
+        <img className="" src={avtr} alt="Display" />
       </nav>
     </div>
   );
