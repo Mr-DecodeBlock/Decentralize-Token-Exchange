edbe5fe7697490008a07c939fc0ca2cb9f44ee04
graphica99 <mendsalbert@gmail.com>
2022-03-31 18:31:46 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="bg-[#1A1D26] w-full h-screen font-Montserrat">
       <div>
-        <nav className="bg-[#20232C] px-4 py-4 sm:w-full lg:w-3/12 lg:h-screen sm:flex sm:flex-row sm:justify-between sm:items-center lg:items-start lg:flex-col">
+        <nav className="bg-[#20232C] px-4 py-4 sm:w-full lg:w-3/12 lg:h-screen sm:flex sm:flex-row sm:justify-between sm:items-center lg:items-start lg:justify-start lg:flex-col ">
           <p className="text-white text-lg">0xafd....88e8f</p>
           <img className="" src={avtr} alt="Display" width={40} />
         </nav>
