3129491775e7af37e6127de8d9673ac4de993f13
graphica99 <mendsalbert@gmail.com>
2022-04-01 13:33:17 +0000

    dex token exchange

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,26 +3,88 @@ import { Dialog, Transition } from "@headlessui/react";
 
 export default function Modal({ open, onClose, children }) {
   return (
-    <div className={`fixed inset-0 ${open ? "" : "pointer-events-none"}`}>
-      {/* backdrop */}
-      <div
-        className={`fixed inset-0 bg-black ${
-          open ? "opacity-50" : "pointer-events-none opacity-0"
-        }`}
-        onClick={onClose}
-      />
+    // <div className={`fixed inset-0 ${open ? "" : "pointer-events-none"}`}>
+    //   {/* backdrop */}
+    //   <div
+    //     className={`fixed inset-0 bg-black ${
+    //       open ? "opacity-50" : "pointer-events-none opacity-0"
+    //     }`}
+    //     onClick={onClose}
+    //   />
 
-      {/* content */}
-      <div
-        className={`fixed right-0 h-full bg-white shadow-lg w-full max-w-screen-sm p-4 ${
-          open ? "opacity-100" : "pointer-events-none opacity-0"
-        }`}
+    //   {/* content */}
+    //   <div className="w-50 h-50 bg-white">
+
+    //     <div>
+    //       <button onClick={onClose}>Click to close modal</button>
+    //     </div>
+    //     {children}
+    //   </div>
+    // </div>
+
+    <Transition appear show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={onClose}
       >
-        <div>
-          <button onClick={onClose}>Click to close modal</button>
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            {/* <Dialog.Overlay className="fixed inset-0" /> */}
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full  max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <Dialog.Title
+                as="h3"
+                className="text-lg font-medium leading-6 text-gray-900"
+              >
+                Payment successful
+              </Dialog.Title>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                  Your payment has been successfully submitted. We’ve sent you
+                  an email with all of the details of your order.
+                </p>
+              </div>
+
+              <div className="mt-4">
+                <button
+                  type="button"
+                  className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                  onClick={onClose}
+                >
+                  Got it, thanks!
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
         </div>
-        {children}
-      </div>
-    </div>
+      </Dialog>
+    </Transition>
   );
 }
