2a82d937ae9f5d99d3e8f274362328257cefe336
graphica99 <mendsalbert@gmail.com>
2022-04-01 14:27:03 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -1,68 +1,17 @@
-import React, { Fragment, useRef, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import avtr from "./avrt-image.png";
-import eth from "./ethereum-eth.svg";
+import React from "react";
+
 import Chart from "react-apexcharts";
 import "./App.css";
 import { chartOptions, dummyData } from "./components/PriceChartConfig";
-import Modal from "./components/Modal";
-import DepositeModal from "./components/DepositeModal";
-import WithdrawModal from "./components/WithdrawModal";
+
 import Layout from "./components/Layout";
+import Sidebar from "./components/Sidebar";
 export default function App() {
   const [openTab, setOpenTab] = React.useState(1);
-  const [open, setOpen] = useState(false);
-  const [comp, setComp] = useState("");
-
-  const cancelButtonRef = useRef(null);
 
   return (
-    // <div className="bg-[#1A1D26] w-full  font-Montserrat">
-    //   <div className="flex flex-col lg:flex lg:flex-row ">
     <Layout>
-      <nav className="bg-transparent  lg:bg-[#20232C] lg:p-4 lg:w-3/12  ">
-        <div className="bg-[#20232C] lg:bg-transparent sm:w-full  px-4 py-4 flex flex-row justify-between items-center lg:flex-col-reverse lg:justify-end lg:py-8 ">
-          <p className="text-white text-lg">0xafd....88e8f</p>
-          <img className="lg:mb-4" src={avtr} alt="Display" width={40} />
-        </div>
-        <hr className="hidden lg:block opacity-20 mt-8" />
-        <div className="lg:block">
-          <div className="mx-4 mt-5 lg:w-full lg:mx-0 lg:block p-4 rounded-md   bg-[#4181DB]">
-            <div className="flex flex-row justify-between items-center">
-              <div className="py-6 text-white">
-                <p className="font-bold">Total Balance</p>
-                <p className="font-bold text-xl">0.00123</p>
-                <p className="bg-yellow-400 font-bold text-black rounded-full text-center w-16">
-                  DDAP
-                </p>
-              </div>
-              <img className="w-20 h-20" src={eth} alt="Display" />
-            </div>
-          </div>
-          <hr className="hidden lg:block opacity-20 mt-8" />
-          <div className="mx-4 lg:mx-0 flex flex-row items-center justify-between space-x-4 lg:flex lg:flex-col lg:space-x-0">
-            <div
-              onClick={() => {
-                setOpen(!open);
-                setComp(<DepositeModal />);
-              }}
-              className="bg-[#FA3E66] py-2 cursor-pointer mt-4 text-white text-lg text-center rounded-md w-full space-y-4"
-            >
-              Deposite
-            </div>
-            <div
-              onClick={() => {
-                setOpen(!open);
-                setComp(<WithdrawModal />);
-              }}
-              className="bg-[#02B156] py-2 cursor-pointer mt-4 text-white text-lg text-center rounded-md w-full space-y-4"
-            >
-              Withdraw
-            </div>
-          </div>
-        </div>
-      </nav>
-
+      <Sidebar />
       <main className=" w-full lg:w-9/12 p-4">
         <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center  lg:py-3">
           <div className="flex flex-row space-x-3 items-center text-xl text-white">
@@ -275,9 +224,6 @@ export default function App() {
           </div>
         </div>
       </main>
-      <Modal open={open} onClose={() => setOpen(false)}>
-        {comp}
-      </Modal>
     </Layout>
   );
 }
