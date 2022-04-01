477b48f6582b82b7766adc4bda4af14699fe1fb7
graphica99 <mendsalbert@gmail.com>
2022-04-01 13:59:15 +0000

    dex token exchange

--- src/components/DepositeModal.js
+++ src/components/DepositeModal.js
@@ -5,12 +5,6 @@ const Comp = () => {
     <div className="text-white flex flex-col">
       <form>
         <div class="mb-6">
-          <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Your email
-          </label>
           <input
             type="email"
             id="email"
@@ -20,12 +14,6 @@ const Comp = () => {
           />
         </div>
         <div class="mb-6">
-          <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Your password
-          </label>
           <input
             type="password"
             id="password"
@@ -33,25 +21,7 @@ const Comp = () => {
             required=""
           />
         </div>
-        <div class="flex items-start mb-6">
-          <div class="flex items-center h-5">
-            <input
-              id="remember"
-              aria-describedby="remember"
-              type="checkbox"
-              class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-              required=""
-            />
-          </div>
-          <div class="ml-3 text-sm">
-            <label
-              for="remember"
-              class="font-medium text-gray-900 dark:text-gray-300"
-            >
-              Remember me
-            </label>
-          </div>
-        </div>
+
         <button
           type="submit"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -62,13 +62,13 @@ export default function Modal({ open, onClose, children }) {
               <div className="mt-2">{children}</div>
 
               <div className="mt-4">
-                <button
+                {/* <button
                   type="button"
                   className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
                   onClick={onClose}
                 >
                   Got it, thanks!
-                </button>
+                </button> */}
               </div>
             </div>
           </Transition.Child>
