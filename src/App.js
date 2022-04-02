7deb195d87e176e0f83712d7d497d3afbc1821b6
graphica99 <mendsalbert@gmail.com>
2022-04-02 00:46:16 +0000

    dex token exchange

--- src/App.js
+++ src/App.js
@@ -33,8 +33,6 @@ export default function App() {
     setAccount(address);
     setExchange(exchangeContract);
     setToken(tokenContract);
-    console.log(exchangeContract);
-    console.log(tokenContract);
   };
   return (
     <Layout>
