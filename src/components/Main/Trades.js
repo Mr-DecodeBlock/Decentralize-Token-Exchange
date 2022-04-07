3cc5fc653be01f96c5c95cbe670b141a25bbf357
graphica99 <mendsalbert@gmail.com>
2022-04-07 06:14:53 +0000

    token exchange app

--- src/components/Main/Trades.js
+++ src/components/Main/Trades.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { filledOrdersLoaded } from "../../store/actions";
+// import { filledOrdersLoaded } from "../../store/actions";
 import {
   filledOrdersLoadedSelector,
   filledOrdersSelector,
