e427e090cf3ed20b168ed97f622ffc856049e4e8
graphica99 <mendsalbert@gmail.com>
2022-04-11 11:51:05 +0000

    portfolio

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -26,30 +26,26 @@ import Exchange from "../artifacts/contracts/Exchange.sol/Exchange.json";
 import { ETHER_ADDRESS } from "../helpers";
 
 export const loadWeb3 = async (dispatch) => {
-  // if (typeof window.ethereum !== "undefined") {
-  //   const web3 = new Web3(window.ethereum);
+  if (typeof window.ethereum !== "undefined") {
+    const web3 = new Web3(window.ethereum);
+    // await web3.ethereum.enable();
 
-  //   // const web3 = new Web3('https://rpc-mumbai.maticvigil.com');
-  //   dispatch(web3Loaded(web3));
-  //   return web3;
-  // } else {
-  //   window.alert("Please install MetaMask");
-  //   window.location.assign("https://metamask.io/");
-  // }
-  if (window.ethereuem) {
-    window.web3 = new Web3(window.ethereum);
-    await window.ethereuem.enable();
-    dispatch(web3Loaded(window.web3));
-  } else if (window.web3) {
-    window.web3 = new Web3(window.web3.currentProvider);
-    dispatch(web3Loaded(window.web3));
+    dispatch(web3Loaded(web3));
+
+    return web3;
   } else {
-    window.alert("no ethereum browser detected");
+    window.alert("Please install MetaMask");
+    window.location.assign("https://metamask.io/");
   }
 };
 
 export const loadAccount = async (web3, dispatch) => {
-  const accounts = await web3.eth.getAccounts();
+  // const accounts = await web3.eth.getAccounts();
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+  // setAccounts(accounts);
+  console.log(accounts);
   const account = accounts[0];
   console.log(account);
   dispatch(web3AccountLoaded(account));
